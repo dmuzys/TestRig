fcd3603f3f94de36f0b3a2ad16d13b1018e802fa
dmuzys <dmuzys@163.com>
2017-11-05 08:13:20 +0800

    添加C#工程

---
+++ TestRig/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace TestRig
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.retButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // retButton
+            // 
+            this.retButton.Font = new System.Drawing.Font("宋体", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.retButton.Location = new System.Drawing.Point(298, 110);
+            this.retButton.Name = "retButton";
+            this.retButton.Size = new System.Drawing.Size(162, 75);
+            this.retButton.TabIndex = 0;
+            this.retButton.Text = "返回";
+            this.retButton.UseVisualStyleBackColor = true;
+            this.retButton.Click += new System.EventHandler(this.retButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(892, 315);
+            this.Controls.Add(this.retButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button retButton;
+    }
+}
+
